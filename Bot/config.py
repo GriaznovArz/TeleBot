4c77eaa1d1506ef06ee0cc7c3562d5c0a705374f
Nikolai <100714944+GriaznovArz@users.noreply.github.com>
2023-01-07 18:24:49 +0300

    Add files via upload

---
+++ Bot/config.py
@@ -0,0 +1,46 @@
+TOKEN = '5702824358:AAGlyTaf-TujxVXRVVk2Qe6VwozDKnumTR4'
+
+headers= {
+  "apikey": '9NG6nzdJts1eyZA84IUGR8VuugvBy4Md'
+}
+
+exchanges = {
+'(AED)United Arab Emirates Dirham': 'AED',
+ '(AUD)Australian Dollar': 'AUD',
+    '(BTC)Bitcoin': 'BTC',
+    '(BYN)New Belarusian Ruble': 'BYN',
+    '(CHF)Swiss Franc': 'CHF',
+    '(CNY)Chinese Yuan': 'CNY',
+    '(COP)Colombian Peso': 'COP',
+    '(CUP)Cuban Peso': 'CUP',
+    '(CZK)Czech Republic Koruna': 'CZK',
+    '(DKK)Danish Krone': 'DKK',
+    '(EGP)Egyptian Pound': 'EGP',
+    '(EUR)Euro': 'EUR',
+    '(GBP)British Pound Sterling': 'GBP',
+    '(HKD)Hong Kong Dollar': 'HKD',
+    '(ILS)Israeli New Sheqel': 'ILS',
+    '(JPY)Japanese Yen': 'JPY',
+    '(KPW)North Korean Won': 'KPW',
+    '(KRW)South Korean Won': 'KRW',
+    '(LKR)Sri Lankan Rupee': 'LKR',
+    '(LVL)Latvian Lats': 'LVL',
+    '(MAD)Moroccan Dirham': 'MAD',
+    '(MDL)Moldovan Leu': 'MDL',
+    '(MNT)Mongolian Tugrik': 'MNT',
+    '(MXN)Mexican Peso': 'MXN',
+    '(NOK)Norwegian Krone': 'NOK',
+    '(NZD)New Zealand Dollar': 'NZD',
+    '(PKR)Pakistani Rupee': 'PKR',
+    '(PLN)Polish Zloty': 'PLN',
+    '(RUB)Russian Ruble': 'RUB',
+    '(SEK)Swedish Krona': 'SEK',
+    '(SGD)Singapore Dollar': 'SGD',
+    '(SYP)Syrian Pound': 'SYP',
+    '(TRY)Turkish Lira': 'TRY',
+    '(TZS)Tanzanian Shilling': 'TZS',
+    '(UAH)Ukrainian Hryvnia': 'UAH',
+    '(USD)United States Dollar': 'USD',
+    '(UZS)Uzbekistan Som': 'UZS',
+    '(ZWL)Zimbabwean Dollar': 'ZWL'
+}
